9a060972dffd3c3ef80eb89a317ebc4507fae62e
RickyHankey <hankey52@students.rowan.edu>
2022-03-23 13:59:57 -0400

    Added Edit Profile page and made the button redirect there. Only logged in users see Edit Profile button. Messed with profile symbols.

--- sprecpro/views.py
+++ sprecpro/views.py
@@ -65,6 +65,39 @@ def profile(request, user_id):
   
   #if the user doesn't exist, go back to welcome
   return redirect('welcome')
+
+def editpr(request, user_id):
+  #If the user is not logged in
+  if not request.user.is_authenticated:
+    return redirect('login')
+
+  #If not a spotify user (super user), redirect to welcome
+  if request.user.is_staff:
+    return redirect('welcome')
+
+  #Find the user's profile who's id matches the passed id in the route
+  #Find the user whos's id was passed in the route, and validate they arent a super user
+  profile = Profile.objects.filter(user_id = user_id).first()
+  user = User.objects.filter(id = user_id, is_staff = False).first()
+
+  #If there is no profile and the user exists, create a profile
+  if user is not None:
+    if profile is None:
+      Profile.objects.create(user_id = request.user, avatar = getProfilePhoto(request.user))
+    
+    #get the users most recently played song and set uid field
+    try:
+      song_data = getUserSongData(request.user)
+      setattr(profile, 'last_played_uid', song_data['last_played'])
+      profile.save()
+      #return the view with the edit profile interface
+      return render(request, 'editpr.html', {'profile' : profile, 'top_song': song_data['top_song']})
+    except:
+      #return the view with the edit interface (no top song)
+      return render(request, 'editpr.html', {'profile' : profile, 'top_song': song_data['top_song']})
+  
+  #if the user doesn't exist, go back to welcome
+  return redirect('welcome')
   
 def getProfilePhoto(user):
   #access the users spotify id and access token
